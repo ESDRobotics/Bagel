0a96db77dae4bc86773af74bbe38bd98691f2e57
Rellek72 <thomashasaemail@gmail.com>
2017-03-22 19:33:16 -0500

    Messing with values
    Trying to improve consistency. Did it work? Only turm wil tell

--- bagel.c
+++ bagel.c
@@ -5,76 +5,81 @@
 int main() {
     create_disconnect();
     create_connect();
-    
+    shut_down_in(119);
     set_servo_position(0, 700);
     enable_servos();
     
-    set_lifter(high - 100, 100);
-    set_lifter(high + 50, 30);
+    set_lifter(high, 100);
     
     create_turn(45, 100, clockwise);
     create_drive_straight(100);
-    msleep(1700);
+    msleep(1850);
     create_stop();
     
     create_turn(45, 100, counter_clockwise);
 
-    while(get_create_lbump() == 0 && get_create_lbump() == 0) { create_drive_straight(100); }
+    while((get_create_lbump() == 0 && get_create_lbump() == 0) && (get_create_rbump() == 0 && get_create_rbump() == 0)) { create_drive_straight(100); }
     create_stop();
 
     create_drive_straight(0);
     msleep(200);
-	create_turn(5,100, clockwise);//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+	create_turn(7,100, clockwise);//Begin picking up the three haybales//////////////
     create_stop();
     
     set_claw(1550, fast);
-    msleep(500);
-
-    set_lifter(mid, 100);
-    set_lifter(low + 20, 20);
-    msleep(500);
+    
+    set_lifter(mid - 300, 100);
+    set_lifter(low+60, 70);
+    msleep(10);
     
     set_claw(800, fast);
-    msleep(500);
+    msleep(10);
+    
     set_claw(1400, fast);
-    msleep(500);
-    
-    set_lifter(low + 10, 20);
-    
+    msleep(100);
+    //set_lifter(low+40, 70);
     set_claw(600, fast);
-    msleep(500);
-    set_claw(1200, fast);
-    msleep(500);
-    set_claw(200, fast);
-    msleep(500);
-    set_claw(1000, fast);
-    msleep(500);
-    set_claw(100, fast);
-    msleep(200);
-
-    set_lifter(high + 50, 100);
+    msleep(10);
+    create_drive_straight(-100);//drive backwards//////////////////
+    msleep(400);
+    create_stop();
     
-	create_turn(5,100, counter_clockwise);//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+    set_claw(1200, fast);
+    set_lifter(low+20, 100);
+    msleep(100);
+    set_claw(200, fast);
+    msleep(10);
+    set_claw(1200, fast);
+    set_lifter(low, 100);
+    msleep(100);
+    set_claw(100, fast);
+    msleep(400);
+
+    set_lifter(high + 30, 100);
+    //create_drive_straight(0);
+    //msleep(400);
+    create_stop();
+    
+	create_turn(7,100, counter_clockwise);//Finished picking up the three haybales, back up and turn///////
 
     create_drive_straight(-100);
     msleep(700);
 
     create_turn(88,100,clockwise);
     //create_turn(490, 60, counter_clockwise);
-    msleep(500);
+    msleep(10);
 
     //drive forward and lower claw
-    create_drive_distance(150, 600);
-    msleep(200);
-    set_lifter(mid, 70);
-    create_drive_distance(100, 600);
+    create_drive_distance(150, 1200);
     msleep(100);
+    set_lifter(mid, 100);
+    msleep(10);
     create_drive_straight(0);
     msleep(100);
     create_stop();
 
     //Turn right into the drop location
-    create_turn(80, 100, clockwise);
+    create_turn(82, 100, clockwise);
 
     //drive backward, lower claw, drive foreard and open servor to drop hay bales
     create_drive_straight(-100);
@@ -83,14 +88,14 @@ int main() {
     create_drive_straight(100);
     msleep(1200);
     create_stop();
+    set_lifter(-180, 100);
     set_lifter(-220, 100);
-    set_lifter(-280, 100);
-    set_claw(1000, slow);  
+    set_claw(1000, fast);  
     set_lifter(420, 100);
     create_drive_straight(-100);
     msleep(1400);  
     create_stop();  
-    set_claw(600, slow);
+    set_claw(600, fast);
     create_drive_straight(100);
     msleep(400);
     
@@ -111,12 +116,12 @@ int main() {
 
     //back up, open claw, lower the claw, grab a single cube
     set_claw(500, medium);
-    set_lifter(low - 20, 50);
+    set_lifter(low - 50, 100);
     create_stop();
     set_claw(100, fast);
     
     //raise claw, and reverse previous commands
-    set_lifter(medium + 200, 70);
+    set_lifter(medium + 200, 700);
     
     create_drive_straight(100);
     msleep(1000);
@@ -126,59 +131,52 @@ int main() {
     create_stop();
 
     //turn towards drop location again and drop
-    create_turn(65, 100, clockwise);
+    create_turn(70, 100, clockwise);
     create_drive_straight(-100);
     msleep(1700);
     create_stop();
-    create_turn(5, 100, clockwise);
-    set_lifter(-20, 50);
-    set_lifter(-100, 50);
-    set_claw(1000, slow); 
+    //create_turn(10, 100, clockwise);
+    set_lifter(60, 100);
+    set_claw(1000, fast); 
     set_lifter(420, 100);
     create_turn(5, 100, counter_clockwise);
+    set_claw(2400, fast);
     create_drive_straight(-100);
     msleep(300);
-    create_stop();
+    create_stop();//go get blue ball
     
-    while(a_button() == 1){msleep(10);}
+    create_turn(216, 100, counter_clockwise);
     
-    set_claw(2400, fast);
-    
-    create_turn(215, 100, counter_clockwise);
-    
-    while(a_button() == 1){msleep(10);}
     create_drive_straight(-100);
-    msleep(2000);
+    msleep(1500);
     create_stop();
-    
-    set_lifter(950, 100);
+    set_lifter(850, 100);
+    set_lifter(910, 100);
     
     set_claw(500, fast);
     
     create_drive_straight(100);
-    msleep(2650);    
+    msleep(2500);    
     
     set_claw(100, fast);
-    msleep(550);
+    msleep(10);
     
     create_drive_straight(-100);
-    msleep(2650);    
+    msleep(2500);    
     
-    create_turn(175, 100, counter_clockwise);
-    
-    set_lifter(950, 100);
-    freeze(0);
+    create_turn(167, 100, counter_clockwise);
     
     create_drive_straight(100);
-    msleep(3650);
+    msleep(3300);
+    
         
-    set_claw(800, fast);
+    set_claw(1400, fast);
     
     msleep(200);
     
     
     create_drive_straight(-100);
-    msleep(3000);
+    msleep(400);
     
     create_disconnect();
 
